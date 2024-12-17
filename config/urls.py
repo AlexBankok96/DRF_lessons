87c0020dd78167a5433faa5727f84a28f4bfe13c
Alex Serg <Sergienko_15@mail.ru>
2024-12-17 15:03:36 +0300

    fix bags

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,12 @@ router.register(r'courses', CourseViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
+    # path('api/v1/', include(router.urls)),
     path('api/v1/lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
     path('api/v1/lessons/<int:pk>/', LessonRetrieveView.as_view(), name='lesson-retrieve'),
     path('api/v1/users/', UserListView.as_view(), name='user-list'),
     path('api/v1/users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/courses/', include('courses.urls')),
 ]
--- courses/tests.py
+++ courses/tests.py
@@ -11,6 +11,6 @@ class CourseAPITests(APITestCase):
 
     def test_course_subscription(self):
         self.client.force_authenticate(user=self.user)
-        response = self.client.post('/subscriptions/', {'course_id': self.course.id})
+        response = self.client.post('/api/v1/courses/subscriptions/', {'course_id': self.course.id})
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data['message'], "Subscription added")
